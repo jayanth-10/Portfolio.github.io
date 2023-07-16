0908988ddb58174c18c954966b4bfaca2f61af2f
Thota Jayanth <105880711+jayanth-10@users.noreply.github.com>
2023-07-16 20:00:52 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".multiple-text",{
+  strings: ["Cyber Security Engineer","Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  loop: true
+})
+
+
+
+
+  
